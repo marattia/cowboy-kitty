0c50510955a48a2ceccec830717ef184f7aef037
marattia <maraziamattialuciano@outlook.it>
2017-03-17 20:43:11 +0100

    Add files via upload

---
+++ Somma di due numeri.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int A,B,S;
+main ()
+{
+	printf("Inserisci il primo valore   \n");
+	scanf("%d",&A);
+	printf("Inserisci il secondo valore   \n");
+	scanf("%d",&B);
+	S=A+B;
+	printf("%d e' il risultato\n",S);
+	system ("pause");
+}
